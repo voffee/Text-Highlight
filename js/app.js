3a7ed935d30b15080561982fda4ddf55d5ad43a1
Val <60014136+voffee@users.noreply.github.com>
2025-03-20 22:26:27 +0800

    Basic card JS completed with bugs.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 
 // Declaring variables
+const firstCard = document.querySelector('.card-1-container');
+const secondCard = document.querySelector('.card-2-container');
 
 // Creating event listeners
+firstCard.addEventListener('click', turnCard);
+secondCard.addEventListener('click', turnCard);
 
 // Creating functions
+function turnCard(e) {
+    console.log('Element contains: ' + e.currentTarget.classList);
+    e.currentTarget.classList.toggle("is-flipped");
+}
+// Test Alerts
 
-// Test Alert
 // alert('test');
